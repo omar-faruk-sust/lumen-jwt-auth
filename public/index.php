46125ef0a29b13471bafe52b78cc4584f191240e
omar.faruk <omar.faruk@shorify.io>
2018-09-05 12:01:09 -0400

    add code for cross origin on header

--- public/index.php
+++ public/index.php
@@ -11,6 +11,16 @@
 |
 */
 
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    exit(0);
+}
+
+
 $app = require __DIR__.'/../bootstrap/app.php';
 
 /*
